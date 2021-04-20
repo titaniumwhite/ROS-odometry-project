8f4acb59410e38091b294d609ee7a6d51b288ec7
Gabriele De Santis <gabriele1.desantis@gmail.com>
2021-04-20 19:30:53 +0200

    first compilation

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -1,15 +1,15 @@
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <robotics_hw1/MotorSpeed.h>
 
 /* Do we really need a policy??? Idk... */
-//typedef message_filters::sync_policies::ExactTime<std_msgs::Float64, std_msgs::Float64, std_msgs::Float64, std_msgs::Float64> MySyncPolicy;
+//typedef message_filters::sync_policies::ExactTime<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed> MySyncPolicy;
 
-void callback(const std_msgs::Float64ConstPtr& msg1, const std_msgs::Float64ConstPtr& msg2,
-              const std_msgs::Float64ConstPtr& msg3, const std_msgs::Float64ConstPtr& msg4) {
+void callback(const robotics_hw1::MotorSpeed::ConstPtr& msg1, const robotics_hw1::MotorSpeed::ConstPtr& msg2,
+              const robotics_hw1::MotorSpeed::ConstPtr& msg3, const robotics_hw1::MotorSpeed::ConstPtr& msg4) {
 
-  ROS_INFO ("Received: [%f, %f, %f, %f]", msg1->data, msg2->data, msg3->data, msg4->data);
+  //ROS_INFO ("Received: [%f, %f, %f, %f]", msg1, msg2, msg3, msg4);
 }
 
 int main(int argc, char** argv) {
@@ -17,13 +17,13 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle n;
 
-    message_filters::Subscriber<std_msgs::Float64> sub1(n, "/speed_fl", 1);
-    message_filters::Subscriber<std_msgs::Float64> sub2(n, "/speed_fr", 1);
-    message_filters::Subscriber<std_msgs::Float64> sub3(n, "/speed_rl", 1);
-    message_filters::Subscriber<std_msgs::Float64> sub4(n, "/speed_rr", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub1(n, "/speed_fl", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub2(n, "/speed_fr", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub3(n, "/speed_rl", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub4(n, "/speed_rr", 1);
   
-    message_filters::TimeSynchronizer<std_msgs::Float64, std_msgs::Float64, 
-                                      std_msgs::Float64, std_msgs::Float64> sync(sub1, sub2, sub3, sub4, 10);
+    message_filters::TimeSynchronizer<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, 
+                                      robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed> sync(sub1, sub2, sub3, sub4, 10);
     
     sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4));
 
