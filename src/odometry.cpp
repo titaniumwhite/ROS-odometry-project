32ef9c3e9bb24c7cdd066147fb88bc4a1e504179
Gabriele De Santis <gabriele1.desantis@gmail.com>
2021-04-20 21:45:48 +0200

    fixing topics motor_speed

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -9,7 +9,7 @@
 void callback(const robotics_hw1::MotorSpeed::ConstPtr& msg1, const robotics_hw1::MotorSpeed::ConstPtr& msg2,
               const robotics_hw1::MotorSpeed::ConstPtr& msg3, const robotics_hw1::MotorSpeed::ConstPtr& msg4) {
 
-  //ROS_INFO ("Received: [%f, %f, %f, %f]", msg1, msg2, msg3, msg4);
+  ROS_INFO ("Received: [%f, %f, %f, %f]", msg1->rpm, msg2->rpm, msg3->rpm, msg4->rpm);
 }
 
 int main(int argc, char** argv) {
@@ -17,10 +17,10 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle n;
 
-    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub1(n, "/speed_fl", 1);
-    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub2(n, "/speed_fr", 1);
-    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub3(n, "/speed_rl", 1);
-    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub4(n, "/speed_rr", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub1(n, "motor_speed_fl", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub2(n, "motor_speed_fr", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub3(n, "motor_speed_rl", 1);
+    message_filters::Subscriber<robotics_hw1::MotorSpeed> sub4(n, "motor_speed_rr", 1);
   
     message_filters::TimeSynchronizer<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, 
                                       robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed> sync(sub1, sub2, sub3, sub4, 10);
