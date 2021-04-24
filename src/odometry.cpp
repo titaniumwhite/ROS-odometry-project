2eaaa4a4b7cc5e6d2616793a67163c34b941f61b
Gabriele De Santis <gabriele1.desantis@gmail.com>
2021-04-24 16:53:30 +0200

    some fixes; starting to implement skid steering

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -10,12 +10,18 @@
 #define RADIUS 0.1575
 #define BASELINE 28.3
 
-typedef struct data {
-  double speed_fl;
-  double speed_fr;
-  double speed_rl;
-  double speed_rr;
-} Data;
+typedef struct pose {
+  double x;
+  double y;
+  double theta;
+} Pose;
+
+typedef struct rpm {
+  double fl;
+  double fr;
+  double rl;
+  double rr;
+} Wheels_rpm;
 
 class twist_stamped {
 private:
@@ -48,27 +54,39 @@ public:
 
 };
 
+class skid_steering {
+
+private:
+  Pose current_pose;
+
+  double linear_velocity;
+  double angular_velocity;
+
+  ros::NodeHandle skid_steering_node;
+  ros::Publisher skid_steering_pub;
+
+public:
 
 
-void angular_velocity_estimator(Data *data, double *linear_velocity, double *angular_velocity){
-  /* TO DO
-  float apparent_baseline;                      // È UNA COSTANTE CHE VA STIMATA
-  // aparent_baseline = (2 * y0), where y0 is the difference between the position of the right center
-  // of istantaneous rotation and the left center of istantaneous rotation */
-  
+};
+
+
+void angular_velocity_estimator(Wheels_rpm *rpm, double *linear_velocity, double *angular_velocity){
+
   // get an average left wheels rpm, also taking into account the reduction gear
-  double left_wheel_avg_rpm  = (data->speed_fl + data->speed_rl) / (2 * GEAR_RATIO);
-  double right_wheel_avg_rpm = (data->speed_fr + data->speed_rr) / (2 * GEAR_RATIO);
+  double left_wheel_avg_rpm  = (rpm->fl + rpm->rl) / (2 * GEAR_RATIO);
+  double right_wheel_avg_rpm = (rpm->fr + rpm->rr) / (2 * GEAR_RATIO);
 
   double left_avg_velocity  = left_wheel_avg_rpm  * RADIUS * RPM_TO_RADS;
   double right_avg_velocity = right_wheel_avg_rpm * RADIUS * RPM_TO_RADS;
 
   *linear_velocity = (left_avg_velocity + right_avg_velocity) / (2.0);
 
-  //double angular_velocity = ( right_avg_velocity + left_avg_velocity ) / (BASELINE); //estimate apparent_baseline
   //double left_angular_velocity = (linear_velocity - (-BASELINE) * angular_velocity) / (RADIUS);
   //double right_angular_velocity = (linear_velocity - BASELINE * angular_velocity) / (RADIUS); //estimate apparent_baseline
-  *angular_velocity = 12321.0;
+  //*angular_velocity = 12321.0;
+  *angular_velocity = ( right_avg_velocity + left_avg_velocity ) / (BASELINE); //estimate apparent_baseline
+
 
   //ROS_INFO ("Linear velocity is : [%f]", linear_velocity);  
   //ROS_INFO ("My angular velocity : [%f]\n", angular_velocity);                                                                
@@ -77,21 +95,21 @@ void angular_velocity_estimator(Data *data, double *linear_velocity, double *ang
 
 void callback(const robotics_hw1::MotorSpeed::ConstPtr& msg1, const robotics_hw1::MotorSpeed::ConstPtr& msg2,
               const robotics_hw1::MotorSpeed::ConstPtr& msg3, const robotics_hw1::MotorSpeed::ConstPtr& msg4, 
-              const nav_msgs::Odometry::ConstPtr& msg5, Data *data, twist_stamped *my_twist_stamped) {
+              const nav_msgs::Odometry::ConstPtr& msg5, Wheels_rpm *rpm, twist_stamped *my_twist_stamped) {
   
   double linear_velocity;
   double angular_velocity;
 
-  data->speed_fl = msg1->rpm;
-  data->speed_fr = msg2->rpm;
-  data->speed_rl = msg3->rpm;
-  data->speed_rr = msg4->rpm;
+  rpm->fl = msg1->rpm;
+  rpm->fr = msg2->rpm;
+  rpm->rl = msg3->rpm;
+  rpm->rr = msg4->rpm;
 
-  //ROS_INFO("Their angular velocity: [%f]", msg5->twist.twist.angular.z);
+  ROS_INFO("Their angular velocity: [%f]", msg5->twist.twist.angular.z);
   angular_velocity_estimator(data, &linear_velocity, &angular_velocity);
   my_twist_stamped->publish_twist_stamped(&linear_velocity, &angular_velocity);
 
-  ROS_INFO ("Linear velocity is : [%f]", linear_velocity);  
+  //ROS_INFO ("Linear velocity is : [%f]", linear_velocity);  
   ROS_INFO ("My angular velocity : [%f]\n", angular_velocity); 
 }
 
@@ -110,7 +128,6 @@ int main(int argc, char** argv) {
     message_filters::Subscriber<robotics_hw1::MotorSpeed> sub4(sync_node, "motor_speed_rr", 1);
     message_filters::Subscriber<nav_msgs::Odometry> sub5(sync_node, "scout_odom", 1);
 
-  
     message_filters::TimeSynchronizer<robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, 
                                       robotics_hw1::MotorSpeed, robotics_hw1::MotorSpeed, 
                                       nav_msgs::Odometry> sync(sub1, sub2, sub3, sub4, sub5, 10);
